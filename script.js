169f89189f71694e1a616d66e6ca7cf4900a0f73
Magic <91306218+MagicFirework@users.noreply.github.com>
2022-08-04 04:30:16 +0600

    add render

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 'use strict';
+const postInfo = document.querySelector('.post-info');
+const getInfo = document.querySelector('.get-info');
+let i = 1;
 
 const getData = () => {
   return fetch('db.json').then(response => response.json());
@@ -14,32 +17,19 @@ const sendData = user => {
   }).then(response => response.json());
 };
 
-const showLocalData = object => {
-  const getInfo = document.querySelector('.get-info');
-  let i = 1;
-
+const render = (object, path) => {
+  i = 1;
   for (const property in object) {
     const elem = document.createElement('span');
     elem.textContent = `${i++} ${property}: ${object[property]}`;
-    getInfo.append(elem);
+    path.append(elem);
   }
 };
 
-const showGlobalData = object => {
-  const postInfo = document.querySelector('.post-info');
-  let i = 1;
-
-  for (const property in object) {
-    const elem = document.createElement('span');
-    elem.textContent = `${i++} ${property}: ${object[property]}`;
-    postInfo.append(elem);
-  }
-};
-
-// Get and Show local Data
-getData().then(object => showLocalData(object));
-
-// Get, Send local Data and Show Global Data
 getData()
+  .then(object => {
+    render(object, getInfo);
+    return object;
+  })
   .then(json => sendData(json))
-  .then(object => showGlobalData(object));
+  .then(object => render(object, postInfo));
