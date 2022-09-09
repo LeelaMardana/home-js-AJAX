30842811293186574396223e9e812da75aba1cfe
AwesemoTowa <Deankurumi@gmail.com>
2022-09-09 21:03:13 +0600

    added button and loader

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 'use strict';
 const postInfo = document.querySelector('.post-info');
 const getInfo = document.querySelector('.get-info');
+const button = document.querySelector('.button');
 let i = 1;
 
+const toggleLoader = () => {
+  if (button.textContent === 'Start') {
+    button.textContent = 'Loading...';
+    button.disabled = true;
+  } else {
+    button.textContent = 'Start';
+    button.disabled = false;
+  }
+};
+
 const getData = () => {
-  return fetch('db.json').then(response => response.json());
+  return fetch('db.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Не удалось принять данные');
+      }
+      return response.json();
+    })
+    .catch(error => console.log(error));
 };
 
 const postData = ({ user, url }) => {
@@ -14,7 +32,14 @@ const postData = ({ user, url }) => {
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
-  }).then(response => response.json());
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Не удалось отправить данные');
+      }
+      return response.json();
+    })
+    .catch(error => console.log(error));
 };
 
 const render = (object, path) => {
@@ -26,12 +51,27 @@ const render = (object, path) => {
   }
 };
 
-getData()
-  .then(object => {
-    render(object, getInfo);
-    return object;
-  })
-  .then(json =>
-    postData({ user: json, url: 'https://jsonplaceholder.typicode.com/posts' })
-  )
-  .then(object => render(object, postInfo));
+let array = [];
+
+button.addEventListener('click', () => {
+  array.forEach(item => item.remove()) || [];
+  toggleLoader();
+  getData()
+    .then(object => {
+      render(object, getInfo);
+      return object;
+    })
+    .then(json =>
+      postData({
+        user: json,
+        url: 'https://jsonplaceholder.typicode.com/posts',
+      })
+    )
+    .then(object => render(object, postInfo))
+    .finally(() => {
+      toggleLoader();
+      const a = postInfo.querySelectorAll('span');
+      const b = getInfo.querySelectorAll('span');
+      array = [...a, ...b];
+    });
+});
